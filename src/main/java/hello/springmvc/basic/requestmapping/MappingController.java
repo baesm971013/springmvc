22d4f6e8937e60a0fea2aa12cd2f9c298f1281b8
baesm971013 <bobbyb537@naver.com>
2024-04-10 11:01:37 +0900

    add RequestMapping example

---
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,40 @@
+package hello.springmvc.basic.requestmapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MappingController {
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping(value = "/hello-basic")
+    public String helloBasic(){
+        log.info("basic");
+        return "ok";
+    }
+    @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET)
+    public String mappingGetV1(){
+        log.info("mapping get v1");
+        return "ok";
+    }
+    @GetMapping("/mapping-get-v2")
+    public String mappingGetV2(){
+        log.info("mapping get v2");
+        return "ok";
+    }
+
+    // 경로변수로 사용하는 추세가 많이 늘어난다
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable String userId){
+        log.info("mapping path userId = {}", userId);
+        return "ok";
+    }
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPathV2(@PathVariable String userId, @PathVariable Long orderId){
+        log.info("userId = {} , orderId = {}", userId, orderId);
+        return "ok";
+    }
+
+
+}
