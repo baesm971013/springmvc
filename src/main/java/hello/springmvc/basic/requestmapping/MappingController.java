5d809fffc75c4051f81c43a7e367c57dfd611e09
baesm971013 <bobbyb537@naver.com>
2024-12-25 17:54:55 +0900

    request mapping

---
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,38 @@
+package hello.springmvc.basic.requestmapping;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+public class MappingController {
+
+    @RequestMapping(value = "/hello-basic", method = RequestMethod.GET)
+    public String helloBasic(){
+        log.info("helloworld");
+        return "hello";
+    }
+
+    @GetMapping("/mapping-get-v2")
+    public String mappingV2(){
+        log.info("hellowrold");
+        return "ok";
+    }
+
+    /**
+     *
+     * @param data
+     * @return
+     */
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data){
+        log.info(data);
+        return "ok";
+    }
+
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPathV2(@PathVariable String userId, @PathVariable String orderId){
+        log.info("order name : {}, order id :{}", userId, orderId);
+        return "ok";
+    }
+}
